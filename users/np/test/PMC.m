bae626c78b1df082ed8459aca5495c0919643105
Nikhil Padmanabhan <nikhil.padmanabhan@gmail.com>
2015-02-06 22:22:53 -0500

    Clean up code somewhat

---
+++ users/np/test/PMC.m
@@ -0,0 +1,78 @@
+(* ::Package:: *)
+
+BeginPackage["PMC`"]
+
+(* A Population Monte Carlo code for Gaussian Mixture Models. This is a direct implementation of arXiv:0903.0837 *)
+
+buildModels::usage="buildModels[ll] translates a list of (wt, mu, covar) into a list of (wts,MultinormalDistribution). This is useful for later use in the code"
+
+mkRandom::usage="mkRandom[model,n,fiddle] generates n random variates from the model. The model is assumed to be the output of buildModels. The fiddle option allows you
+to fiddle with the output eg. to transform things etc"
+
+iterPMC::usage="Iterate the PMC code for a Gaussian Mixture model"
+
+Begin["`Private`"]
+
+Clear[buildModels,mkRandom];
+buildModels[ll_]:=Module[{wt,models},
+  wt = ll[[All,1]];
+  models = MultinormalDistribution@@@ ll[[All,2;;3]];
+  {wt,models}
+];
+
+mkRandom[ll_,n_,fiddle_:# &]:= Module[{xx},
+  xx=RandomVariate[MixtureDistribution@@ll, n];
+  fiddle[xx]
+]
+
+(* The major iteration code is below *)
+
+iterPMC[chi2_, npop_, model_,fiddle_:# &] :=
+    Module[ {perplex,xx,lik,pps,totpp,wt,mix,reg,eps,dets},
+	(* The PMC loop starts here, execute this cell until the perplexity gets close to 1 *)
+	(* Generate npop randoms and calculate the posterior at these points*)
+        perplex = 0;
+        While[perplex < 0.95,
+         (* Generate random numbers *)
+         xx = mkRandom[model, npop];
+         xx = {Mod[#1, 1], #2, #3} & @@@ xx;
+         lik = chi2 @@@ xx;
+         lik = lik - Min[lik];
+         lik = Exp[-lik/2];
+
+         (* Now compute the PDF for each of the mixtures and weight*)
+         pps = PDF[#, xx] & /@ model[[2]];
+         pps = pps*model[[1]];
+         totpp = Total[pps, 1];
+         pps = #/totpp & /@ pps;
+         
+         (* Compute weights *)
+         wt = lik/totpp;
+         wt = wt/Total[wt];
+
+         (* Perplexity *)
+         perplex = Exp[-Total[wt Log[wt]]]/npop;
+
+         (* Now do the update step *)
+         mix = Module[ {a, mu, sig, dx, dx1},
+                   a = Total[wt* #];
+                   mu = Total[wt*#*xx, 1]/a;
+                   dx = # - mu & /@ xx;
+                   dx1 = dx*wt*#;
+                   sig = (Transpose[dx1].dx)/a + reg*eps;
+                   {a, mu, sig}
+               ] & /@ pps;
+         dets = Det[#] & /@ mix[[All, 3]];
+         If[ (Length@Select[dets, # <= 0 &]) > 0,
+             Print["Determinant Problem"];
+             Break[]
+         ];
+         reg = reg/1.2;
+         model = buildModels[mix];
+         ];
+        {perplex, xx, model}
+    ]
+
+
+End[]
+EndPackage[]
