ff1991f2519c45822acbb3cbc3f13f0c08aef7e2
Nikhil Padmanabhan <nikhil.padmanabhan@gmail.com>
2015-02-07 17:56:09 -0500

    Code cleanup

--- users/np/test/PMC.m
+++ users/np/test/PMC.m
@@ -9,33 +9,36 @@ buildModels::usage="buildModels[ll] translates a list of (wt, mu, covar) into a 
 mkRandom::usage="mkRandom[model,n,fiddle] generates n random variates from the model. The model is assumed to be the output of buildModels. The fiddle option allows you
 to fiddle with the output eg. to transform things etc"
 
-iterPMC::usage="Iterate the PMC code for a Gaussian Mixture model"
+iterPMC::usage="iterPMC[chi2,npop,model,eps,fiddle] iterates the PMC code for a Gaussian Mixture model. eps allows one to regularize the
+covariance matrix generated, and prevent it from blowing up"
 
 Begin["`Private`"]
 
 Clear[buildModels,mkRandom];
 buildModels[ll_]:=Module[{wt,models},
   wt = ll[[All,1]];
+  wt = wt/Total[wt]; (* Normalize inside here *)
   models = MultinormalDistribution@@@ ll[[All,2;;3]];
   {wt,models}
 ];
 
-mkRandom[ll_,n_,fiddle_:# &]:= Module[{xx},
+mkRandom[ll_,n_,fiddle_]:= Module[{xx},
   xx=RandomVariate[MixtureDistribution@@ll, n];
-  fiddle[xx]
+  fiddle@@@xx
 ]
 
 (* The major iteration code is below *)
 
-iterPMC[chi2_, npop_, model_,fiddle_:# &] :=
-    Module[ {perplex,xx,lik,pps,totpp,wt,mix,reg,eps,dets},
+iterPMC[chi2_, npop_, modelin_,eps_,fiddle_] :=
+    Module[ {perplex,xx,lik,pps,totpp,wt,mix,reg,dets,model},
 	(* The PMC loop starts here, execute this cell until the perplexity gets close to 1 *)
 	(* Generate npop randoms and calculate the posterior at these points*)
+	    model = modelin;
         perplex = 0;
+        reg=1.0;
         While[perplex < 0.95,
          (* Generate random numbers *)
-         xx = mkRandom[model, npop];
-         xx = {Mod[#1, 1], #2, #3} & @@@ xx;
+         xx = mkRandom[model, npop,fiddle];
          lik = chi2 @@@ xx;
          lik = lik - Min[lik];
          lik = Exp[-lik/2];
